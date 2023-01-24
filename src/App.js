034182ab33af9956a8b631552f19d6ba12ea6cdd
Annie <overheads165@gmail.com>
2023-01-24 19:45:01 +0100

    color changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import Form from './Form';
-import styled from 'styled-components';
+import "./App.css";
+import Form from "./Form";
+import styled from "styled-components";
+import React, { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("#456389");
+
   return (
-    <Background color = "#000">
-    <Form/>
+    <Background color={color}>
+      <Form color={color} setColor={setColor} />
     </Background>
   );
 }
- 
+
 const Background = styled.div`
-background-color: ${props => props.color};
-align-items: center;
-display: flex;
-height: 100vh;
-justify-content: center;
-width: 100vw;
-`
+  background-color: ${(props) => props.color};
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+`;
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 
-const Form = () => {
-  const [color, setColor] = useState("");
+const Form = ({setColor, color}) => {
   const handlerChangeColor = (event) => {
     setColor(event.target.value);
   };
   return (
     <>
       <form>
-        <input value={color} onChange={handlerChangeColor} />
+        <input value = {color} onChange={handlerChangeColor} />
       </form>
     </>
   );
 };
-
 
 
 export default Form;
